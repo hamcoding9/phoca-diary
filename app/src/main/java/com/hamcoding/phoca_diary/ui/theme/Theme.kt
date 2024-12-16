6dffe56f09c582164a057b662ba746663e06af3a
hamcoding9 <dientes8520@gmail.com>
2024-12-16 22:35:41 +0900

    add colorSet

--- app/src/main/java/com/hamcoding/phoca_diary/ui/theme/Theme.kt
+++ app/src/main/java/com/hamcoding/phoca_diary/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.hamcoding.phoca_diary.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +9,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import com.hamcoding.phoca_diary.ui.theme.color.Pink40
+import com.hamcoding.phoca_diary.ui.theme.color.Pink80
+import com.hamcoding.phoca_diary.ui.theme.color.Purple40
+import com.hamcoding.phoca_diary.ui.theme.color.Purple80
+import com.hamcoding.phoca_diary.ui.theme.color.PurpleGrey40
+import com.hamcoding.phoca_diary.ui.theme.color.PurpleGrey80
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
